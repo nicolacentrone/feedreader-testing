96bcb4873b84cdea19c09568ce5649ca78212156
Nicola Centrone <nicola.centrone.it@gmail.com>
2018-09-20 22:28:04 +0200

    fix: Update code to ES6 syntax

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,13 +9,13 @@
  * to ensure they don't run until the DOM is ready.
  */
 
-$(function() {
+$(() => {
 
   /* This is our first test suite - a test suite just contains
   * a related set of tests. This suite is all about the RSS
   * feeds definitions, the allFeeds variable in our application.
   */
-  describe('RSS Feeds', function() {
+  describe('RSS Feeds', () => {
 
     /* This is our first test - it tests to make sure that the
      * allFeeds variable has been defined and that it is not
@@ -24,7 +24,7 @@ $(function() {
      * allFeeds in app.js to be an empty array and refresh the
      * page?
      */
-    it('are defined', function() {
+    it('are defined', () => {
       expect(allFeeds).toBeDefined();
       expect(allFeeds.length).not.toBe(0);
     });
@@ -32,8 +32,8 @@ $(function() {
     /* Checks if the attribute url of the objects within allFeeds are
      * defined and not empty
      */
-    it('has an URL defined and not empty', function() {
-      allFeeds.forEach(function(obj) {
+    it('has an URL defined and not empty', () => {
+      allFeeds.forEach((obj) => {
         expect(obj.url).toBeDefined();
         expect(obj.url).not.toBe('');
       });
@@ -42,8 +42,8 @@ $(function() {
     /* Checks if the attribute name of the objects within allFeeds are
      * defined and not empty
      */
-    it('has a name defined and not empty', function() {
-      allFeeds.forEach(function(obj) {
+    it('has a name defined and not empty', () => {
+      allFeeds.forEach((obj) => {
         expect(obj.name).toBeDefined();
         expect(obj.name).not.toBe('');
       });
@@ -52,19 +52,19 @@ $(function() {
 
   /* This suite checks the menu functionality */
 
-  describe('The menu', function() {
+  describe('The menu', () => {
 
     /* Checks if the menu is hidden by default. The CSS class .menu-hidden then
      * must be present in the <body>
      */
-    it('is hidden by default', function() {
+    it('is hidden by default', () => {
       expect($('body').hasClass('menu-hidden')).toBe(true);
     });
 
     /* Checks if the menu toggles on-off if clicked, by simulating a click with
      * the .click() method from HTMLElement Web API
     */
-    it('toggles when clicked', function() {
+    it('toggles when clicked', () => {
       let trigger = document.querySelector('.menu-icon-link');
       trigger.click();
       expect($('body').hasClass('menu-hidden')).toBe(false);
@@ -75,14 +75,14 @@ $(function() {
 
   /* This test suite checks the feeds list.
   */
-  describe('Initial Entries', function() {
-    beforeEach(function(done) { // beforeEach is launched before any test
+  describe('Initial Entries', () => {
+    beforeEach((done) => { // beforeEach is launched before any test
       loadFeed(0, done); // we need to load the first feed (index 0)
     });
 
   /* Checks if at least one feed .entry CSS class is present
    */
-    it('have at least one entry element', function(done) {
+    it('have at least one entry element', (done) => {
       let feed = document.querySelector('.feed .entry');
       expect($(feed).hasClass('entry')).toBe(true);
       done();
@@ -91,11 +91,11 @@ $(function() {
 
   /* Checks the feed content.
   */
-  describe('New Feed Selection', function() {
+  describe('New Feed Selection', () => {
     let html1;
     let html2;
-    beforeEach(function(done) {
-      loadFeed(0, function() {
+    beforeEach((done) => {
+      loadFeed(0, () => {
         html1 = document.querySelector('.feed').innerHTML;
         loadFeed(1, done);
       });
@@ -104,7 +104,7 @@ $(function() {
     /* Checks if the content of the feed changes by actually checking if the
      * innerHTML string changes
      */
-    it('has its content that actually changes', function(done) {
+    it('has its content that actually changes', (done) => {
       html2 = document.querySelector('.feed').innerHTML;
       expect(html1).not.toBe(html2);
       done();
